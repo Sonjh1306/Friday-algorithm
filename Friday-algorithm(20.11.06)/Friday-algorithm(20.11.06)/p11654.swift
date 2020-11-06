48c8d2dbdb1a66f9660c4a79df0f0f668d7f9211
Sonjh1306 <yale1306@gmail.com>
2020-11-06 17:38:27 +0900

    완료

--- Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/p11654.swift
+++ Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/p11654.swift
@@ -6,3 +6,12 @@
 //
 //백준 11654번
 import Foundation
+
+func asciiConverter() -> Int{
+    print("입력하세요")
+    let input = readLine()!
+    let ch = Character(input)
+    let asciiValue = ch.asciiValue!
+    let result = Int(asciiValue)
+    return result
+}
