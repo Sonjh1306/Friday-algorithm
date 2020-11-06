47521d0cb3e2764e21edfd677c76183349d1f375
Sonjh1306 <yale1306@gmail.com>
2020-11-06 17:41:18 +0900

    진행중

--- Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/main.swift
+++ Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/main.swift
@@ -14,6 +14,9 @@ import Foundation
 //print()
 //printStar()
 //print()
-codeScanner()
+//codeScanner()
 //print()
-
+//print(calculatorOX())
+//print()
+//print(asciiConverter())
+//print()
--- Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/p8958.swift
+++ Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/p8958.swift
@@ -6,3 +6,33 @@
 //
 // 백준 8958번
 import Foundation
+
+//func calculatorOX() -> Int {
+//    print("입력하세요")
+//    let input = readLine()!
+//    var inputArray : [String] = []
+//    
+//    for i in input {
+//        inputArray.append(String(i))
+//    }
+//    inputArray.append(String("X"))
+//    
+//    let count = inputArray.count
+//    
+//    var score = 0
+//    var bonus = 0
+//    
+//    for i in 0..< count - 1{
+//        if inputArray[i] == "O" {
+//            score += 1
+//            if inputArray[i+1] == "O"{
+//                bonus += 1
+//                score += bonus
+//            }else{
+//                bonus = 0
+//            }
+//        }
+//        
+//    }
+//    print(score)
+//}
