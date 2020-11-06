fd7395e3b75255e2ede2a9e32be4814c768bf207
Sonjh1306 <yale1306@gmail.com>
2020-11-06 15:38:18 +0900

    진행중

---
+++ Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/p2920.swift
@@ -0,0 +1,8 @@
+//
+//  p2920.swift
+//  Friday-algorithm(20.11.06)
+//
+//  Created by sonjuhyeong on 2020/11/06.
+//
+// 백준 2920번
+import Foundation
---
+++ Friday-algorithm(20.11.06)/Friday-algorithm(20.11.06)/p8958.swift
@@ -0,0 +1,8 @@
+//
+//  p8958.swift
+//  Friday-algorithm(20.11.06)
+//
+//  Created by sonjuhyeong on 2020/11/06.
+//
+// 백준 8958번
+import Foundation
