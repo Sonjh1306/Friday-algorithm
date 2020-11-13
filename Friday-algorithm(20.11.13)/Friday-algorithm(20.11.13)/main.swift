f5f671a95ba8e3b75fdad98ba0ec5d81b00ba3ba
Sonjh1306 <yale1306@gmail.com>
2020-11-13 11:56:27 +0900

    -

---
+++ Friday-algorithm(20.11.13)/Friday-algorithm(20.11.13)/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Friday-algorithm(20.11.13)
+//
+//  Created by sonjuhyeong on 2020/11/12.
+//
+
+import Foundation
+
+
+//print(solution12910(_arr: [1,2,3,4,5], _divisor: 5))
+
+//print(solution_12928(5))
