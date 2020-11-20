fc1016133670a5ed0167861f0e1a6a753d9c64a2
Sonjh1306 <yale1306@gmail.com>
2020-11-20 17:37:05 +0900

    -

---
+++ Firday-algorithm(2020.11.20)/Friday-algorithm(2020.11.20)/Number_Line_Jumps  .swift
@@ -0,0 +1,13 @@
+//
+//  Number_Line_Jumps  .swift
+//  Firday-algorithm(2020.11.20)
+//
+//  Created by sonjuhyeong on 2020/11/20.
+//
+
+import Foundation
+
+func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
+
+
+}
--- Firday-algorithm(2020.11.20)/Friday-algorithm(2020.11.20)/main.swift
+++ Firday-algorithm(2020.11.20)/Friday-algorithm(2020.11.20)/main.swift
@@ -9,9 +9,9 @@ import Foundation
 
 //print(solveMeFirst(a: 3, b: 5 ))
 
-print(gradingStudents(grades: [12,24,43,60]))
+//print(gradingStudents(grades: [12,24,43,60]))
 
-print(compareTriplets(a: [1,2,3], b: [2,3,3]))
+//print(compareTriplets(a: [1,2,3], b: [2,3,3]))
 
-print(aVeryBigSum(ar: [1,2,3,4]))
+//print(aVeryBigSum(ar: [1,2,3,4]))
 
