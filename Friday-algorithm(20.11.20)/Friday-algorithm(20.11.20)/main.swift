dcd1259a4e002dd051bfdf1115bb2cf9e7e56284
Sonjh1306 <yale1306@gmail.com>
2020-11-20 12:17:59 +0900

    -

---
+++ Friday-algorithm(20.11.20)/Friday-algorithm(20.11.20)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Friday-algorithm(20.11.20)
+//
+//  Created by sonjuhyeong on 2020/11/20.
+//
+
+import Foundation
+
+//print(solveMeFirst(a: 3, b: 5 ))
+
+print(gradingStudents(grades: [12,24,43,60]))
